c8ccf111959a66f57ee6b9ba1aabe3a9211d60c9
Dominik Goltermann <support@tiberianwars.de>
2011-12-09 10:52:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlarmDb::Application.config.secret_token = '900dc652e85d510f3e6145200956a6443c80b8ee2e343200054e086b93c7493f7f1392c57ba330b8caca2836b3afc3fa1d5718b7338ed30ab25583db0419c397'
