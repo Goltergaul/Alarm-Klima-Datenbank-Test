803ead1664569289aed3a4560a83e264467a97a8
Hubert Hoelzl <info@sketchit.de>
2012-01-16 10:32:26 +0100

    working on png output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,45 +1,75 @@
 module ApplicationHelper
-  def getPNG values, variable
+  def getPNG values, var
+    width = 300
+    height = 300
     min_value = nil
     max_value = nil
     sum = 0
     cnt = 0
     
-    png = ChunkyPNG::Image.new(300, 300, ChunkyPNG::Color::TRANSPARENT)
+    pre = {:variable => "pre", :data => values[:pre]}
+    tmp = {:variable => "tmp", :data => values[:tmp]}
+    gdd = {:variable => "gdd", :data => values[:gdd]}
     
-    pre = values[:pre]
-    tmp = values[:tmp]
-    gdd = values[:gdd]
-    values = values[variable]
+    output_image = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
     
-    # var_arr = [pre,tmp,gdd]
-    # 
-    # var_arr.each do |values|
+    var_arr = [pre,tmp,gdd]
+    images = []
     
-    values.each_with_index do |arr, x|
-      arr.each_with_index do |value, y|
-        next if value.nil?
-        min_value = value if min_value.nil?
-        max_value = value if max_value.nil?
-        min_value = value if value < min_value
-        max_value = value if value > max_value
+    var_arr.each do |hash|
+      values = hash[:data]
+      variable = hash[:variable]
+      next if values.nil?
+      
+      output_image = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
+      images.push(output_image)
+      
+      values.each_with_index do |arr, x|
+        arr.each_with_index do |value, y|
+          next if value.nil?
+          min_value = value if min_value.nil?
+          max_value = value if max_value.nil?
+          min_value = value if value < min_value
+          max_value = value if value > max_value
         
-        cnt = cnt + 1
-        sum = sum + value
-      end unless arr.nil?
+          cnt = cnt + 1
+          sum = sum + value
+        end unless arr.nil?
+      end
+    
+      values.each_with_index do |arr, x|
+        arr.each_with_index do |value, y|
+          #next if value.nil?
+          y = 228 - y
+          output_image[x,y] = getColor(variable, value, min_value, max_value)
+        end unless arr.nil?
+      end
+      
     end
     
-    values.each_with_index do |arr, x|
-      arr.each_with_index do |value, y|
-        next if value.nil?
-        y = 228 - y
-        png[x,y] = getColor(variable, value, min_value, max_value)
-      end unless arr.nil?
+    if var.downcase=="all"
+      output_image = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
+      red=0
+      green=0
+      blue=0
+      (0..257).each do |x|
+        (0..227).each do |y|
+          images.each do |image|
+            color = image.get_pixel(x,y)
+            r = ChunkyPNG::Color.r(color)
+            g = ChunkyPNG::Color.g(color)
+            b = ChunkyPNG::Color.b(color)
+            red = r unless r==0
+            green = g unless g==0
+            blue = b unless b==0
+          end
+          unless red==0 and green==0 and blue==0
+            output_image[x,y] = ChunkyPNG::Color.rgb(red, green, blue)
+          end
+        end
+      end
     end
-    puts "Min value: #{min_value}"
-    puts "Max value: #{max_value}"
-    puts "Average: #{sum / cnt}"
-    png
+    output_image
   end
   
   def getColor variable, value, min, max
@@ -58,11 +88,17 @@ module ApplicationHelper
         green = 255
         blue = 255
     end
+    if value.nil?
+      return ChunkyPNG::Color.rgba(red, green, blue, 0)
+    end
     range = max - min
     percent = value / range
     red = (red*percent).to_i
     green = (green*percent).to_i
     blue = (blue*percent).to_i
+    
+    blue=0
+    green=0
     
     ChunkyPNG::Color.rgb(red, green, blue)
   end
