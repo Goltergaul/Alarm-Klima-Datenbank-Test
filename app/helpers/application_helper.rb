173f3580f77525459fb81997aaf5fbce982f2823
Hubert Hoelzl <info@sketchit.de>
2012-01-18 14:22:20 +0100

    finished png output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,56 +1,51 @@
 module ApplicationHelper
   def getPNG values, var
-    width = 300
-    height = 300
-    min_value = nil
-    max_value = nil
+    width = 259
+    height = 229
+    range = []
     
     pre = {:variable => "pre", :data => values[:pre]}
     tmp = {:variable => "tmp", :data => values[:tmp]}
     gdd = {:variable => "gdd", :data => values[:gdd]}
     
-    output_image = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
-    
     var_arr = [pre,tmp,gdd]
     images = []
+    
+    output_image = nil
     
     var_arr.each do |hash|
       values = hash[:data]
       variable = hash[:variable]
       next if values.nil?
       
-      output_image = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
-      images.push(output_image)
+      png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
       
-      values.each_with_index do |arr, x|
-        arr.each_with_index do |value, y|
-          next if value.nil?
-          min_value = value if min_value.nil?
-          max_value = value if max_value.nil?
-          min_value = value if value < min_value
-          max_value = value if value > max_value
-        end unless arr.nil?
-      end
+      # get min/max values for current variable
+      minmax = getMinMax values
     
       values.each_with_index do |arr, x|
         arr.each_with_index do |value, y|
-          #next if value.nil?
           y = 228 - y
-          output_image[x,y] = getColor(variable, value, min_value, max_value)
+          png[x,y] = getColor(variable, value, minmax)
         end unless arr.nil?
       end
-      
+      images.push(png)
     end
     
-    if var.downcase=="all"
+    # only pre OR tmp OR gdd is used
+    if images.length==1
+      output_image = images.first
+    else
+      # ALL the variables are used -> combine the pngs from the images array
       output_image = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
-      red=0
-      green=0
-      blue=0
       (0..257).each do |x|
         (0..227).each do |y|
+          red=0
+          green=0
+          blue=0
           images.each do |image|
             color = image.get_pixel(x,y)
+            next if color==0
             r = ChunkyPNG::Color.r(color)
             g = ChunkyPNG::Color.g(color)
             b = ChunkyPNG::Color.b(color)
@@ -58,16 +53,39 @@ module ApplicationHelper
             green = g unless g==0
             blue = b unless b==0
           end
+          
           unless red==0 and green==0 and blue==0
-            output_image[x,y] = ChunkyPNG::Color.rgb(red, green, blue)
+            unless red==255 and green==255 and blue==255
+              output_image[x,y] = ChunkyPNG::Color.rgba(red, green, blue, 255)
+            end
           end
+          
         end
       end
     end
+    
+    # return the output_image, either a single var or ALL varshblock
     output_image
   end
   
-  def getColor variable, value, min, max
+  def getMinMax values
+    min_value = nil
+    max_value = nil
+    values.each_with_index do |arr, x|
+      arr.each_with_index do |value, y|
+        next if value.nil?
+        min_value = value if min_value.nil?
+        max_value = value if max_value.nil?
+        min_value = value if value < min_value
+        max_value = value if value > max_value
+      end unless arr.nil?
+    end
+    return [min_value, max_value]
+  end
+  
+  def getColor variable, value, minmax
+    min = minmax[0]
+    max = minmax[1]
     red = 0
     green = 0
     blue = 0
@@ -100,9 +118,6 @@ module ApplicationHelper
     green = (green*percent).to_i
     blue = (blue*percent).to_i
     
-    blue=0
-    green=0
-    
-    ChunkyPNG::Color.rgb(red, green, blue)
+    ChunkyPNG::Color.rgba(red, green, blue, 255)
   end
 end
